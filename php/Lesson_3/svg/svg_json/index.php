47673cc2444e742452b7c5f798f0e04676146b9f
kosankuz <konstantinkuzmenko@icloud.com>
2019-07-06 20:51:00 +0100

    Lessons

---
+++ php/Lesson_3/svg/svg_json/index.php
@@ -0,0 +1,28 @@
+<?php
+require './php/main.php'
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+    <link rel="stylesheet" href="/css/">
+</head>
+<body>
+
+<div>
+
+    <svg>
+        <?php getIcon('facebook');?>
+    </svg>
+<br>
+    <svg>
+        <?php getIcon('baby');?>
+    </svg>
+
+</div>
+
+</body>
+</html>
